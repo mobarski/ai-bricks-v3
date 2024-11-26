7b983b4c1813c3e75bd7ac13ece98cf5fa33b1f4
Maciej Obarski <mobarski@gmail.com>
2024-11-26 22:35:51 +0100

    add llamacpp

---
+++ providers/llamacpp_api.py
@@ -0,0 +1,15 @@
+from openai_api import OpenAiHttpApi
+
+
+# REF: https://github.com/ggerganov/llama.cpp/blob/master/examples/server/README.md
+class LlamaCppHttpApi(OpenAiHttpApi):
+    api_key_env = None
+    api_base_url = "http://127.0.0.1:8080/v1"
+    provider = "llamacpp"
+
+
+if __name__ == "__main__":
+    # TODO: test this
+    model = LlamaCppHttpApi("llama-3.2-1b-instruct@q4_k_m")
+    resp = model.chat_create([{"role": "user", "content": "Tell me a joke."}])
+    print(resp)
