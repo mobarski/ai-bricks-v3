210c5d15b9f9e9056ce693069b642464c4bdefe2
Maciej Obarski <mobarski@gmail.com>
2024-12-09 19:37:04 +0100

    add game v1/v2 config split

--- examples/easy/game/v2.py
+++ examples/easy/game/v2.py
@@ -4,20 +4,22 @@ import aibricks
 import rich
 import re
 from rich.console import Console
-console = Console()
+
+CHARACTER = 'marcus'
+MODEL = 'lmstudio:'
+console = Console(width=80)
 
 
-# TODO: width
 def rich_output(text):
-    text = re.sub('(["][^"]*?["])', r'[blue]\1[/]', text)
-    text = re.sub('([*][^*]*?[*])', r'[yellow]\1[/]', text)
-    rich.print(text)
+    text = re.sub('(["][^"]*?["])', r'[yellow]\1[/]', text)
+    text = re.sub('([*][^*]*?[*])', r'[cyan]\1[/]', text)
+    console.print(text)
 
 
 os.chdir(os.path.dirname(__file__)) # kind of ugly
-cfg = aibricks.load_config("./game.yaml")
+cfg = aibricks.load_config("./game-v2.yaml")
 
-character = cfg.lookup('characters.anne')
+character = cfg.lookup(f'characters.{CHARACTER}')
 game_prompt = cfg.render('game_prompt', character=character)
 
 messages = [{'role': 'system', 'content': game_prompt}]
@@ -32,9 +34,10 @@ summary_middleware = aibricks.middleware.ChatSummaryMiddleware(ctx, max_in_conte
 client.add_middleware(summary_middleware)
 
 while True:
+    rich_output('='*80)
     user_text = console.input("[bold white]You: ", )
     messages += [{'role': 'user', 'content': user_text}]
-    response = client.chat.completions.create(model='lmstudio:', messages=messages)
+    response = client.chat.completions.create(model=MODEL, messages=messages)
     ai_text = response.choices[0].message.content
     rich_output('\n' + ai_text)
     messages += [{'role': 'assistant', 'content': ai_text}]
